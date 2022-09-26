819623f220e3ac934979ef1de1c97685e412bfed
haydenfd <haydenfds@gmail.com>
2022-09-26 03:22:20 -0700

    quick fix

--- src/twitter/client.ts
+++ src/twitter/client.ts
@@ -5,7 +5,7 @@ import { createQueryStringURL } from "../utils";
 export const getAuthClient = () => new auth.OAuth2User({
   client_id: 'c25UXzlnMkFlMm01cHNBY3lsUl86MTpjaQ',
   client_secret: 'Y_SX2dhiAHWgvQKUQbegOsdnB6fzxIf2TCKHHur7N5O3NdUbpx',
-  callback: createQueryStringURL('/api/login'),
+  callback: createQueryStringURL('api/login'),
   scopes: ["users.read", "tweet.read", "bookmark.read"]
 })
 
